3249cbea9e25e77bf3423f96300896c4eaf4a61f
25im0047-commits <25im0047@i-u.ac.jp>
2025-12-05 17:25:02 +0900

    メールアドレスに自分のアドレスを追加した

--- app/sendEmail/mail.tsx
+++ app/sendEmail/mail.tsx
@@ -1,41 +1,40 @@
 "use server";
 
-import {EmailTemplateOwner, EmailTemplateCustemer} from './EmailTemplate';
-import { Resend } from 'resend';
+import { EmailTemplateOwner, EmailTemplateCustemer } from "./EmailTemplate";
+import { Resend } from "resend";
 
-const RESEND_API_KEY = process.env.RESEND_API_KEY
+const RESEND_API_KEY = process.env.RESEND_API_KEY;
 
 const resend = new Resend(RESEND_API_KEY);
 
-
 export async function SEND_TO_OWNER(formData: FormData) {
-  console.log('Form Data received:', formData);
+  console.log("Form Data received:", formData);
   try {
     const { data, error } = await resend.emails.send({
-      from: 'K-PASS無料受験相談予約通知 <onboarding@cramreserveform.com>',
-      to: ['matuott1230@gmail.com'],
-      subject: 'K-PASS 無料受験相談予約通知',
+      from: "K-PASS無料受験相談予約通知 <onboarding@cramreserveform.com>",
+      to: ["matuott1230@gmail.com", "shotakobayashi7@gmail.com"], //取得したドメインに変更
+      subject: "K-PASS 無料受験相談予約通知",
       react: EmailTemplateOwner(formData),
     });
-    console.log('OwnerData:', data, "error", error);
+    console.log("OwnerData:", data, "error", error);
   } catch (error) {
-    console.log('Error sending email to customer:', error);
+    console.log("Error sending email to customer:", error);
   }
 }
 
 export async function SEND_TO_CUSTEMER(formData: FormData) {
-  console.log('Form Data received(custemer):', formData);
-  const email_customer = formData.get('email') as string;
-  console.log('Custemer Email:', email_customer);
+  console.log("Form Data received(custemer):", formData);
+  const email_customer = formData.get("email") as string;
+  console.log("Custemer Email:", email_customer);
   try {
     const { data, error } = await resend.emails.send({
-      from: 'K-PASS無料受験相談予約通知 <onboarding@cramreserveform.com>',
+      from: "K-PASS無料受験相談予約通知 <onboarding@cramreserveform.com>",
       to: [email_customer],
-      subject: 'K-PASS 無料受験相談予約完了通知',
+      subject: "K-PASS 無料受験相談予約完了通知",
       react: EmailTemplateCustemer(formData),
     });
-    console.log('CustomerData:', data, "error", error);
+    console.log("CustomerData:", data, "error", error);
   } catch (error) {
-    console.log('Error sending email to Customer:', error);
+    console.log("Error sending email to Customer:", error);
   }
 }
