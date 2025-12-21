f4bf62c126e9226473f72166b76277a8a998f318
Matsuo Shouta <matuott1230@gmail.com>
2025-12-21 20:16:22 +0900

    コンソール消した

--- app/sendEmail/mail.tsx
+++ app/sendEmail/mail.tsx
@@ -4,12 +4,10 @@ import { EmailTemplateOwner, EmailTemplateCustemer } from "./EmailTemplate";
 import { Resend } from "resend";
 
 const RESEND_API_KEY = process.env.RESEND_API_KEY;
-console.log(RESEND_API_KEY);
 
 const resend = new Resend(RESEND_API_KEY);
 
 export async function SEND_TO_OWNER(formData: FormData) {
-  console.log("オーナーにメールが送信されました");
   try {
     const { data, error } = await resend.emails.send({
       from: "K-PASS <reservation@kpass-form.com>",
@@ -17,14 +15,11 @@ export async function SEND_TO_OWNER(formData: FormData) {
       subject: "K-PASS 無料受験相談予約通知",
       react: EmailTemplateOwner(formData),
     });
-    console.log(error);
   } catch (error) {
-    console.log(error);
   }
 }
 
 export async function SEND_TO_CUSTEMER(formData: FormData) {
-  console.log("オーナーにメールが送信されました");
   const email_customer = formData.get("email") as string;
   try {
     const { data, error } = await resend.emails.send({
@@ -33,8 +28,6 @@ export async function SEND_TO_CUSTEMER(formData: FormData) {
       subject: "K-PASS 無料受験相談予約完了通知",
       react: EmailTemplateCustemer(formData),
     });
-    console.log(error);
   } catch (error) {
-    console.log(error);
   }
 }
