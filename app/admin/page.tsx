ea72ddbbfe001f0ae50f56753a21c234673a95db
25im0047-commits <25im0047@i-u.ac.jp>
2025-12-25 17:48:58 +0900

    adminテスト

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,32 @@
-import Link from "next/link";
+"use client";
 
-export default function admin(){
-    return (<>
-    <h1>admin page</h1>
-    <Link href="/"><button>Home</button></Link>
-    </>);
+import { useState } from "react";
+
+export default function AdminPage() {
+  const [startAt, setStartAt] = useState("");
+  async function createMeet() {
+    const res = await fetch("/api/create-meet", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        startAt, // ← これを渡す
+      }),
+    });
+    const { meetLink } = await res.json();
+    alert(meetLink);
+  }
+
+  return (
+    <>
+      <h1>admin page</h1>
+      <input
+        type="datetime-local"
+        value={startAt}
+        onChange={(e) => setStartAt(e.target.value)}
+      />
+      <button onClick={createMeet} className="border">
+        Create Meet
+      </button>
+    </>
+  );
 }
